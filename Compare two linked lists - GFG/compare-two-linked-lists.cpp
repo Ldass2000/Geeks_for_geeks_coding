c3c03763e7afb94f6bc0653d6d78d07fad7f43f9
Lalu Kumar <83582883+Ldass2000@users.noreply.github.com>
2022-02-26 20:10:46 +0530

    Added solution - LeetHub

---
+++ Compare two linked lists - GFG/compare-two-linked-lists.cpp
@@ -0,0 +1,88 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;  
+// Linked list Node structure
+struct Node
+{
+    char c;
+    struct Node *next;
+    
+    Node(char x){
+        c = x;
+        next = NULL;
+    }
+    
+};  
+
+
+int compare(Node *list1, Node *list2); 
+
+
+int main()
+{
+ int t,n;
+ cin>>t;
+ char x;
+ while(t--)
+ {
+ cin>>n;
+ cin>>x;
+  Node *list1 = new Node(x);
+    Node *temp=list1;
+    for(int i=0;i<n-1;i++){
+			cin>>x;
+		temp->next = new Node(x);
+		temp=temp->next;
+		}
+		int m;
+		cin>>m;
+    cin>>x;
+    Node *list2 = new Node(x);
+    temp=list2;
+    for(int i=0;i<m-1;i++){
+			cin>>x;
+		temp->next = new Node(x);
+		temp=temp->next;
+		}
+ 
+    cout << compare(list1, list2)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+/* Linked list Node structure
+struct Node
+{
+    char c;
+    struct Node *next;
+    
+    Node(char x){
+        c = x;
+        next = NULL;
+    }
+    
+};
+*/
+
+// Compare two strings represented as linked lists
+int compare(Node *list1, Node *list2) 
+{
+    //  Node *p=list1;
+    //  Node *q=list2;
+     while(list1!=NULL && list2!=NULL && list1->c==list2->c){
+         list1=list1->next;
+         list2=list2->next;
+     }
+     if(list1!=NULL && list2!=NULL){
+         return (list1->c > list2->c )? 1:-1;
+     }
+     if(list1==NULL && list2!=NULL){
+         return -1;
+     }
+     if(list1!=NULL && list2==NULL){
+         return 1;
+     }
+     return 0;
+}
